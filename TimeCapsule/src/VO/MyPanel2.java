d18744cb47d7e2bea765bc1c747d4de54091cc6c
pc-05 <pc-05@pc-05-PC>
2017-10-21 23:06:16 +0900

    마이패널2 디자인 대충 완성!

--- TimeCapsule/src/VO/MyPanel2.java
+++ TimeCapsule/src/VO/MyPanel2.java
@@ -15,6 +15,9 @@ import javax.swing.SwingConstants;
 import javax.swing.border.MatteBorder;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import javax.swing.JScrollPane;
 
 public class MyPanel2 extends JPanel {
 	public JLabel lbl_title;
@@ -30,6 +33,10 @@ public class MyPanel2 extends JPanel {
 	private Dimension liked;
 	private JLabel lbl_num;
 	private JLabel lbl_name;
+	private JPanel panel;
+	private JPanel panel_1;
+	private JPanel panel_2;
+	private JPanel panel_3;
 
 	public MyPanel2(SpringLayout sl_pn_scroll, JPanel pn_1) {
 		like = new ImageIcon(".\\Image\\blue.png");
@@ -41,32 +48,11 @@ public class MyPanel2 extends JPanel {
 		sl_pn_scroll.putConstraint(SpringLayout.EAST, this, 643, SpringLayout.WEST, pn_1);
 
 		setOpaque(false);
-
-		SpringLayout sl_pn_2 = new SpringLayout();
-		setLayout(sl_pn_2);
-
-		lbl_title = new JLabel("\uC2A4\uB9C8\uD2B8\uBBF8\uB514\uC5B4\uC778\uC7AC\uAC1C\uBC1C\uC6D0");
-		sl_pn_2.putConstraint(SpringLayout.NORTH, lbl_title, 50, SpringLayout.NORTH, this);
-		sl_pn_2.putConstraint(SpringLayout.WEST, lbl_title, 270, SpringLayout.WEST, this);
-		lbl_title.setHorizontalAlignment(SwingConstants.CENTER);
-		lbl_title.setFont(new Font("a펜글씨B", Font.PLAIN, 30));
-		this.add(lbl_title);
-		
-
-		lbl_howtime = new JLabel("18:27:00'");
-		lbl_howtime.setForeground(Color.GRAY);
-		
-		sl_pn_2.putConstraint(SpringLayout.NORTH, lbl_howtime, 23, SpringLayout.SOUTH, lbl_title);
-		sl_pn_2.putConstraint(SpringLayout.WEST, lbl_howtime, 30, SpringLayout.WEST, this);
-		sl_pn_2.putConstraint(SpringLayout.SOUTH, lbl_howtime, 186, SpringLayout.SOUTH, lbl_title);
-		lbl_howtime.setOpaque(false);
-		lbl_howtime.setHorizontalAlignment(SwingConstants.CENTER);
-		lbl_howtime.setFont(new Font("a엄마의편지B", Font.ITALIC, 18));
-		lbl_howtime.setBackground(Color.WHITE);
-		this.add(lbl_howtime);
+		setLayout(null);
 
 		lbl_string = new JLabel("\uC5F4\uB9AC\uB294 \uC2DC\uAC04");
-		lbl_string.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_string.setBounds(0, 315, 99, 18);
+		lbl_string.setHorizontalAlignment(SwingConstants.LEFT);
 		this.add(lbl_string);
 
 		pn_2_weather = new JPanel() {
@@ -83,25 +69,16 @@ public class MyPanel2 extends JPanel {
 				super.paintComponent(g);
 			}
 		};
-		sl_pn_2.putConstraint(SpringLayout.EAST, lbl_howtime, -55, SpringLayout.EAST, pn_2_weather);
-		sl_pn_2.putConstraint(SpringLayout.EAST, lbl_string, -248, SpringLayout.WEST, pn_2_weather);
-		sl_pn_2.putConstraint(SpringLayout.NORTH, pn_2_weather, 274, SpringLayout.NORTH, this);
-		sl_pn_2.putConstraint(SpringLayout.SOUTH, pn_2_weather, -30, SpringLayout.SOUTH, this);
-		sl_pn_2.putConstraint(SpringLayout.WEST, pn_2_weather, -53, SpringLayout.EAST, this);
-		sl_pn_2.putConstraint(SpringLayout.EAST, pn_2_weather, -10, SpringLayout.EAST, this);
+		pn_2_weather.setBounds(587, 305, 43, 36);
 		this.add(pn_2_weather);
 
 		lbl_showtime = new JLabel();
-		sl_pn_2.putConstraint(SpringLayout.WEST, lbl_showtime, 184, SpringLayout.WEST, this);
-		sl_pn_2.putConstraint(SpringLayout.EAST, lbl_showtime, -136, SpringLayout.WEST, pn_2_weather);
-		sl_pn_2.putConstraint(SpringLayout.SOUTH, lbl_string, -6, SpringLayout.NORTH, lbl_showtime);
-		sl_pn_2.putConstraint(SpringLayout.NORTH, lbl_showtime, 301, SpringLayout.NORTH, this);
+		lbl_showtime.setBounds(111, 315, 223, 18);
 		lbl_string.setHorizontalAlignment(SwingConstants.CENTER);
 		this.add(lbl_showtime);
 		
 		likecount = new JLabel("1000");
-		sl_pn_2.putConstraint(SpringLayout.NORTH, likecount, 6, SpringLayout.SOUTH, pn_2_weather);
-		sl_pn_2.putConstraint(SpringLayout.WEST, likecount, -20, SpringLayout.WEST, pn_2_weather);
+		likecount.setBounds(500, 317, 43, 15);
 		likecount.setHorizontalAlignment(SwingConstants.CENTER);
 		add(likecount);
 		
@@ -120,6 +97,7 @@ public class MyPanel2 extends JPanel {
 				super.paintComponent(g);
 			}
 		};
+		pn_like.setBounds(555, 315, 20, 18);
 		pn_like.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseEntered(MouseEvent e) {
@@ -130,32 +108,64 @@ public class MyPanel2 extends JPanel {
 				like.setImage(like2.getImage());
 			}
 		});
-		
-		
-		
-		sl_pn_2.putConstraint(SpringLayout.NORTH, pn_like, 5, SpringLayout.SOUTH, pn_2_weather);
-		sl_pn_2.putConstraint(SpringLayout.WEST, pn_like, 7, SpringLayout.EAST, likecount);
-		sl_pn_2.putConstraint(SpringLayout.SOUTH, pn_like, 23, SpringLayout.SOUTH, pn_2_weather);
 		pn_like.setPreferredSize(new Dimension(20,30));
 
 
 		add(pn_like);
 		
-		lbl_num = new JLabel("No_1");
-		lbl_num.setHorizontalAlignment(SwingConstants.LEFT);
-		lbl_num.setForeground(Color.GRAY);
-		lbl_num.setFont(new Font("a엄마의편지B", Font.BOLD, 12));
-		sl_pn_2.putConstraint(SpringLayout.NORTH, lbl_num, 10, SpringLayout.NORTH, this);
-		sl_pn_2.putConstraint(SpringLayout.WEST, lbl_num, 15, SpringLayout.WEST, this);
-		add(lbl_num);
+		panel = new JPanel();
+		panel.setOpaque(false);
+		panel.setBounds(15, 34, 636, 52);
+		add(panel);
+		panel.setLayout(new GridLayout(1, 0, 0, 0));
 		
-		lbl_name = new JLabel("\uC784\uC1A1\uD558");
-		lbl_name.setHorizontalAlignment(SwingConstants.RIGHT);
-		lbl_name.setForeground(Color.GRAY);
-		sl_pn_2.putConstraint(SpringLayout.NORTH, lbl_name, 10, SpringLayout.NORTH, this);
-		sl_pn_2.putConstraint(SpringLayout.EAST, lbl_name, -15, SpringLayout.EAST, this);
-		lbl_name.setFont(new Font("a엄마의편지B", Font.BOLD, 12));
-		add(lbl_name);
+				lbl_title = new JLabel("\uC2A4\uB9C8\uD2B8\uBBF8\uB514\uC5B4\uC778\uC7AC\uAC1C\uBC1C\uC6D0");
+				panel.add(lbl_title);
+				lbl_title.setHorizontalAlignment(SwingConstants.CENTER);
+				lbl_title.setFont(new Font("a펜글씨B", Font.PLAIN, 30));
+				
+				panel_1 = new JPanel();
+				panel_1.setOpaque(false);
+				panel_1.setBounds(15, 96, 636, 181);
+				add(panel_1);
+				panel_1.setLayout(new GridLayout(1, 0, 0, 0));
+				
+
+				lbl_howtime = new JLabel("18:27:00'");
+				panel_1.add(lbl_howtime);
+				lbl_howtime.setForeground(Color.GRAY);
+				lbl_howtime.setOpaque(false);
+				lbl_howtime.setHorizontalAlignment(SwingConstants.CENTER);
+				lbl_howtime.setFont(new Font("a엄마의편지B", Font.ITALIC, 18));
+				lbl_howtime.setBackground(Color.WHITE);
+				
+				
+				panel_2 = new JPanel();
+				panel_2.setOpaque(false);
+				FlowLayout flowLayout = (FlowLayout) panel_2.getLayout();
+				flowLayout.setAlignment(FlowLayout.LEFT);
+				panel_2.setBounds(15, 12, 131, 25);
+				add(panel_2);
+				
+				lbl_num = new JLabel("No_1");
+				panel_2.add(lbl_num);
+				lbl_num.setHorizontalAlignment(SwingConstants.LEFT);
+				lbl_num.setForeground(Color.GRAY);
+				lbl_num.setFont(new Font("a엄마의편지B", Font.BOLD, 12));
+				
+				panel_3 = new JPanel();
+				panel_3.setOpaque(false);
+				FlowLayout flowLayout_1 = (FlowLayout) panel_3.getLayout();
+				flowLayout_1.setHgap(10);
+				flowLayout_1.setAlignment(FlowLayout.RIGHT);
+				panel_3.setBounds(504, 11, 147, 26);
+				add(panel_3);
+				
+				lbl_name = new JLabel("\uC784\uC1A1\uD558\uD558\uD558");
+				panel_3.add(lbl_name);
+				lbl_name.setHorizontalAlignment(SwingConstants.RIGHT);
+				lbl_name.setForeground(Color.GRAY);
+				lbl_name.setFont(new Font("a엄마의편지B", Font.BOLD, 12));
 	}
 
 	public void get_icon(String str) {
